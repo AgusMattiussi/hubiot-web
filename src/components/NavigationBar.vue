872e5e5987df18c72e9daee7e9215d04227ec2a2
Matias <mdaneri@itba.edu.ar>
2022-05-06 19:40:30 -0300

    Se agregó los botones faltantes en el navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-toolbar class="primary" dense elevation="2" >
+  <v-toolbar class="primary" dense elevation="2">
     <router-link to="/">
       <v-container>
         <v-img class="img" :src="require('../assets/logo.png')" :alt="logo"/>
@@ -9,6 +9,12 @@
       <v-container>
         <p class="text-h4 white--text pt-3">Hubiot</p>
       </v-container>
+    </router-link>
+    <router-link to="devices" class="ml-4">
+      <v-btn class="primary" elevation="0">Mis dispositivos</v-btn>
+    </router-link>
+    <router-link to="routines" class="ml-2">
+      <v-btn class="primary" elevation="0">Mis rutinas</v-btn>
     </router-link>
     <v-spacer></v-spacer>
     <router-link to="/about">
