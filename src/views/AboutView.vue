d7b3cbd7b30268fa851632fedb67d956222fc081
Matias <mdaneri@itba.edu.ar>
2022-05-18 14:51:42 -0300

    Aboutus hecho.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about fill-height" >
-    <h1>This is an about page</h1>
-  </div>
+  <v-main class="fill-height primary" fluid>
+      <v-card class="primary">
+        <v-card-text>
+          <v-card-title><h1>Integrantes</h1></v-card-title>
+          <h2>Matías E. Daneri</h2><br/>
+          <h2>Agustín H. Mattiussi</h2><br/>
+          <h2>Julián M. Sasso</h2><br/>
+        </v-card-text>
+      </v-card>
+    </v-main>
 </template>
+
+<style scoped></style>
